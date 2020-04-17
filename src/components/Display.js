092286e4e710b030950e6c3cee22001bfee3c193
Rawand Dargali <rdargali@yahoo.com>
2020-04-17 02:33:07 -0500

    hour marker is conditional, working correctly

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,15 +5,15 @@ function Display({ time }) {
     if (time.h === 0) {
       return "";
     } else {
-      return <span>{time.h >= 0 ? time.h : "0" + time.h}</span>;
+      return <span>{time.h >= 0 ? time.h : "0" + time.h} </span>;
     }
   };
   return (
     <div>
-      {conditionalH()} &nbsp;:&nbsp;
-      <span>{time.m >= 0 ? time.m : "0" + time.m}</span>&nbsp;:&nbsp;
-      <span>{time.s >= 0 ? time.s : "0" + time.s}</span>&nbsp;:&nbsp;
-      <span>{time.ms >= 0 ? time.ms : "0" + time.ms}</span>&nbsp;
+      {conditionalH()} &nbsp;
+      <span>{time.m >= 0 ? time.m : "0" + time.m}</span>&nbsp;&nbsp;
+      <span>{time.s >= 0 ? time.s : "0" + time.s}</span>&nbsp;&nbsp;
+      <span>{time.ms >= 0 ? time.ms : "0" + time.ms}</span>&nbsp;&nbsp;
     </div>
   );
 }
