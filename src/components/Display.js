faaadd23b4d6a13770569ebc2445d50fe105be24
Rawand Dargali <rdargali@yahoo.com>
2020-04-17 02:25:42 -0500

    hour marker is conditional

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function Display({ time }) {
+  const conditionalH = () => {
+    if (time.h === 0) {
+      return "";
+    } else {
+      return <span>{time.h >= 0 ? time.h : "0" + time.h}</span>;
+    }
+  };
   return (
     <div>
-      <span>{time.h >= 0 ? time.h : "0" + time.h}</span>&nbsp;:&nbsp;
+      {conditionalH()} &nbsp;:&nbsp;
       <span>{time.m >= 0 ? time.m : "0" + time.m}</span>&nbsp;:&nbsp;
       <span>{time.s >= 0 ? time.s : "0" + time.s}</span>&nbsp;:&nbsp;
       <span>{time.ms >= 0 ? time.ms : "0" + time.ms}</span>&nbsp;
